b5a5977e6f1323a6bee415033855269632262d3e
Gustavo Magaña López <gmaganna.biomed@gmail.com>
2021-07-07 12:56:38 -0500

    :art: removed hardcoded path
    The data dir has now been specified as a relative path using the __file__ attribute instead of a hardcoded path.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,17 @@ import random
 from tidycsv.utils.customobjs import Path
 import pytest
 
+from typing import List
 
-# TODO : find a way to remove hardcoded Path.
+_ROOT_DIR: Path = Path(__file__).parent.absolute().parent.absolute()
+_DATA_DIR: Path = _ROOT_DIR.joinpath("data")
+
+
 @pytest.fixture
 def random_csv_file() -> Path:
-    """get a random csv file from the data directory
-    WARNING : Hardcoded"""
-    data_dir = Path("/home/gml/Proyects/Personal/tidycsv/data")
-    csv_files = data_dir.lglob("*.csv")
-    rnd_index = random.randint(0, len(csv_files) - 1)
+    """get a random csv file from the data directory"""
+    print(_DATA_DIR)
+    data_dir: Path = _DATA_DIR
+    csv_files: List[Path] = data_dir.lglob("*.csv")
+    rnd_index: int = random.randint(0, len(csv_files) - 1)
     return csv_files[rnd_index]
