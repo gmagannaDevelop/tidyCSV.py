0d0b18269c67d1abcae007ef501fe3257f92c8c6
Gustavo Magaña López <gmaganna.biomed@gmail.com>
2021-07-24 12:37:14 -0500

    :bulb: added docstrings
    .

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -3,6 +3,6 @@ from tidycsv.utils.customobjs import Path
 
 
 def test_get_csv_counts_type(random_csv_file):
-    """ Assert that for any given csv file, the object returned is a dict. """
+    """Assert that for any given csv file, the object returned is a dict."""
     print(Path(".").abs)
     assert isinstance(get_csv_counts(random_csv_file), dict)
--- tidycsv/core/parsing.py
+++ tidycsv/core/parsing.py
@@ -1,17 +1,48 @@
+"""
+"""
+
+from io import StringIO
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Union
 from functools import reduce
 
+__all__ = ["get_csv_counts"]
 
-def get_csv_counts(file: Path):
-    """ """
-    with open(file.absolute().as_posix(), "r") as f:
-        lines = f.readlines()
+
+def get_csv_counts(file: Union[str, Path]) -> Dict[int, List[str]]:
+    """
+    Get groups of semantically consistsent
+
+    Parameters
+    ----------
+        file : a string, pathlib.Path to used within a
+               call to `open(file, "r") as f`.
+
+    Returns
+    -------
+        A dictionnary containing the number of commas as keys
+        and the lines which have said number of commas.
+        i.e.
+
+            {
+                3: [
+                    "x1,x2,x3",
+                    "11,21,31",
+                    "12,22,33",
+                ],
+                2: [
+                    "extrainfo,date",
+                    "none,2020-05-05"
+                ]
+            }
+    """
+    with open(file, "r") as f:
+        lines: List[str] = f.readlines()
 
     csv_counts: Dict[int, List[str]] = {}
 
     for line in lines:
-        n_commas = line.count(",")
+        n_commas: int = line.count(",")
         if n_commas in csv_counts.keys():
             csv_counts[n_commas].append(line)
         else:
@@ -19,4 +50,7 @@ def get_csv_counts(file: Path):
 
     return csv_counts
 
+
+def get_biggest_():
+    pass
     # return reduce(lambda x, y: x if len(x[1]) > len(y[1]) else y, csv_counts.items())
