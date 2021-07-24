029ae6ab2924199cb1486cfffb67c0df415ef4f9
Gustavo Magaña López <gmaganna.biomed@gmail.com>
2021-07-24 16:26:36 -0500

    :building_construction: added minimal interface to the package
    .

--- tidycsv/__init__.py
+++ tidycsv/__init__.py
@@ -1,1 +1,9 @@
-__version__ = '0.1.0'
+"""
+A minimalistic solution to messy csv files.
+"""
+
+from .core import parsing
+
+__version__ = "0.1.0"
+__author__ = "Gustavo Magaña López"
+__credits__ = "Gustavo Magaña López"
